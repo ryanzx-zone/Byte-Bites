621b79ad2f4a782055a71af7b71d5097a1e092ae
ryanzx-zone <rpangzx@gmail.com>
2025-05-21 15:45:01 +0800

    Create App.js

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image, Button, SafeAreaView, Platform} from 'react-native';
+import Mywelcomescreen from './app/screens/Mywelcomescreen';
+//import MyViewImageScreen from './app/screens/MyViewImageScreen';
+
+export default function App() {
+  return (
+    <Mywelcomescreen />
+    //<MyViewImageScreen />
+    
+    
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    //alignItems: 'center',
+    //justifyContent: 'center',
+    //paddingTop: Platform.OS === "web" ? StatusBar.currentheight : 0,
+  },
+});
